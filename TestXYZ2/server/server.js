fb33960d8e7be33081ec84d8870d99d8b1292470
Prashanth Narayanaswamy <prashanth.gedde@gmail.com>
2015-11-07 11:04:19 -0800

    Inception

---
+++ TestXYZ2/server/server.js
@@ -0,0 +1,25 @@
+var loopback = require('loopback');
+var boot = require('loopback-boot');
+
+var app = module.exports = loopback();
+
+// boot scripts mount components like REST API
+boot(app, __dirname);
+
+app.start = function() {
+  // start the web server
+  return app.listen(process.env.PORT || 3000, function() {
+    app.emit('started');
+    var baseUrl = app.get('url').replace(/\/$/, '');
+    console.log('Web server listening at: %s', baseUrl);
+    if (app.get('loopback-component-explorer')) {
+      var explorerPath = app.get('loopback-component-explorer').mountPath;
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+    }
+  });
+};
+
+// start the server if `$ node server.js`
+if (require.main === module) {
+  app.start();
+}
